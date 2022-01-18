14cd2d969c9c8d9aa7c53a6db51f458a5cab21ed
DalalAlsaidi <dalal.alsaidi.has.s@tuwaiq.edu.sa>
2022-01-18 18:40:12 +0300

    Test1

--- Testone/ViewController.swift
+++ Testone/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
 
-
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
